2677b8a3da5c8225a3ff6c41cd2d773821708e09
Domantas Jurkus <domantas.jurkus@gmail.com>
2019-03-28 11:46:34 +0000

    Fix sliding window

--- main_sliding_window.py
+++ main_sliding_window.py
@@ -1,24 +1,16 @@
-import numpy as np
 import torch
-import torch.nn.functional as F
+import numpy as np
 
 from datasets.xtion1video import Xtion1VideoDataset
 from models.lstm_sliding_window import LSTMSlidingWindow
-from train_test_sliding_window import *
-
-# https://pytorch.org/docs/stable/notes/randomness.html
-torch.manual_seed(1337)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+# from train_test_video import *
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # device = "cpu"
 print(device)
 
 BATCH_SIZE = 1
-SHUFFLE = True
 FRAMES_PER_SEQUENCE = 6
-STRIDE = 1
 
 classes = ('pant', 'shirt', 'sweater', 'towel', 'tshirt')
 n_classes = len(classes)
@@ -26,24 +18,11 @@ n_classes = len(classes)
 dataset_train = Xtion1VideoDataset(root='../project-data/continuous_masked')
 dataset_test = Xtion1VideoDataset(root='../project-data/continuous_masked_test')
 
-train_params = {
-    "batch_size": BATCH_SIZE,
-    "num_workers": 8,
-    "shuffle": True,
-}
-
-test_params = {
-    "batch_size": BATCH_SIZE,
-    "num_workers": 8,
-    "shuffle": True,
-}
-
-loader_train = torch.utils.data.DataLoader(dataset_train, **train_params)
-loader_test = torch.utils.data.DataLoader(dataset_test, **test_params)
+loader_train = torch.utils.data.DataLoader(dataset_train, batch_size=BATCH_SIZE, num_workers=8, shuffle=True)
+loader_test = torch.utils.data.DataLoader(dataset_test, batch_size=BATCH_SIZE, num_workers=8, shuffle=True)
 
 def get_model():
     model = LSTMSlidingWindow(n_classes, device=device)
-    # model.load_state_dict(torch.load('saved_models/lstmslidingwindow_masked_epoch14_acc0.400000.pt'))
     return model
 
 model = get_model()
@@ -53,13 +32,7 @@ training_losses = []
 testing_losses = []
 accuracies = []
 
-def init_hidden_and_cell(hidden_size=128, device="cpu"):
-    # hard-coded batch size of 1
-    h0 = torch.zeros(1, 1, hidden_size, device=device)
-    c0 = torch.zeros(1, 1, hidden_size, device=device)
-    return (h0, c0)
-
-def train(model, train_loader, test_loader, n_classes, epochs=10, masked=False, save=False, hidden_size=128, fps=6, device="cpu"):
+def train(model, train_loader, test_loader, n_classes, epochs=10, masked=False, save=False, device="cpu"):
     minibatch_count = len(train_loader)
     print('training minibatch count:', minibatch_count)
     print("device:", device)
@@ -76,22 +49,14 @@ def train(model, train_loader, test_loader, n_classes, epochs=10, masked=False, 
             videos, targets = videos.to(device, dtype=torch.float), targets.to(device)
             model.optimizer.zero_grad()
 
-            batch, timesteps, _, _, _ = videos.size()
+            outputs = model(videos)
 
-            hc = init_hidden_and_cell(hidden_size, device)
+            loss = model.criterion(outputs, targets)
+            loss.backward()
+            model.optimizer.step()
 
-            for end in range(fps, timesteps+1, STRIDE):
-                feature_sequence = videos[:, end-fps:end, :]
-                outputs, hc = model(feature_sequence, hc)
-                hc = (hc[0].detach(), hc[1].detach())
-
-                # backpropagate for each snippet
-                loss = model.criterion(outputs, targets)
-                loss.backward()
-                model.optimizer.step()
-
-                total_loss += loss.item()
-                running_loss += loss.item()
+            total_loss += loss.item()
+            running_loss += loss.item()
 
             if i % 5 == 0:
                 print('epoch: %d minibatches: %d/%d loss: %.3f' % (epoch, i, minibatch_count, running_loss))
@@ -101,8 +66,8 @@ def train(model, train_loader, test_loader, n_classes, epochs=10, masked=False, 
         acc = test(model, test_loader, n_classes, TEST_LOSS_MULTIPLY, device=device)
         print('Total training loss:', total_loss)
 
-        if save:
-            save_model(model, masked, epoch, acc)
+        # if save:
+        #     save_model(model, masked, epoch, acc)
 
         print('Training losses:', str(training_losses).replace(",", "").replace("[", "").replace("]", ""))
         print('Testing losses:', str(testing_losses).replace(",", "").replace("[", "").replace("]", ""))
@@ -110,13 +75,13 @@ def train(model, train_loader, test_loader, n_classes, epochs=10, masked=False, 
 
     print('Finished Training')
 
-def save_model(model, masked, epoch, acc):
-    model_name = type(model).__name__.lower()
-    is_masked = 'masked' if masked else 'depth'
-    torch.save(model.state_dict(), "saved_models/%s_%s_epoch%d_acc%f.pt" % (model_name, is_masked, epoch, acc))
-    print("model saved")
+# def save_model(model, masked, epoch, acc):
+#     model_name = type(model).__name__.lower()
+#     is_masked = 'masked' if masked else 'depth'
+#     torch.save(model.state_dict(), "saved_models/%s_%s_epoch%d_acc%f.pt" % (model_name, is_masked, epoch, acc))
+#     print("model saved")
 
-def test(model, test_loader, n_classes, TEST_LOSS_MULTIPLY, fps=6, device="cpu"):
+def test(model, test_loader, n_classes, TEST_LOSS_MULTIPLY, device="cpu"):
     model.eval()
     correct = 0
     total = 0
@@ -134,41 +99,24 @@ def test(model, test_loader, n_classes, TEST_LOSS_MULTIPLY, fps=6, device="cpu")
         for i, data in enumerate(test_loader):
             videos, targets, _ = data
             videos, targets = videos.to(device, dtype=torch.float), targets.to(device)
-            batch_size, timesteps, _, _, _ = videos.size()
 
-            hc = init_hidden_and_cell(device=device)
-            # prediction_bins = torch.zeros([1, n_classes], device=device)
+            outputs = model(videos)
 
-            # slide window with STRIDE
-            for k in range(fps, timesteps+1, STRIDE):
-                feature_sequence = videos[:, k-fps:k, :]
-                outputs, hc = model(feature_sequence, hc)
-
-                softmax_predictions = F.softmax(outputs, dim=1)
-                # prediction_bins += softmax_predictions
-
-                # backpropagate for each snippet
-                loss = model.criterion(outputs, targets) * TEST_LOSS_MULTIPLY
-
-                total_loss += loss.item()
-                running_loss += loss.item()
-
-            #TODO: extend with accuracy metric (correctly classified snippets / number of snippets)
-            #TODO: the same for single shot, but correctly classified frames/ number of frames
-
-            # pick index with max activation value
-            # _, predicted_indexes = torch.max(prediction_bins.data, 1)
-
-            # outputs of the last block
-            _, predicted_indexes = torch.max(outputs, 1)
+            loss = model.criterion(outputs, targets) * TEST_LOSS_MULTIPLY
+            total_loss += loss.item()
+            running_loss += loss.item()
+            
+            # max pool over time
+            _, predicted_indexes = torch.max(outputs.data, 1)
             
             # bin predictions into confusion matrix
-            for j in range(batch_size):
+            for j in range(len(videos)):
                 actual = targets[j].item()
                 predicted = predicted_indexes[j].item()
                 confusion[actual][predicted] += 1
 
             # sum up total correct
+            batch_size = targets.size(0)
             total += batch_size
             correct_vector = (predicted_indexes == targets)
             correct += correct_vector.sum().item()
@@ -196,4 +144,5 @@ def test(model, test_loader, n_classes, TEST_LOSS_MULTIPLY, fps=6, device="cpu")
     return acc
 
 if __name__ == '__main__':  
-    train(model, loader_train, loader_test, n_classes, epochs=50, save=True, masked=True, device=device)
+    train(model, loader_train, loader_test, n_classes, epochs=50, save=False, masked=True, device=device)
+
--- models/lstm_sliding_window.py
+++ models/lstm_sliding_window.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+STRIDE = 6
+
 def get_feature_extractor():
     return nn.Sequential(
         nn.Conv2d(1, 12, kernel_size=11, stride=4),
@@ -25,21 +27,20 @@ class LSTMSlidingWindow(nn.Module):
         self.device = device
         self.fps = fps
         self.lstm_hidden_size = lstm_hidden_size
-        self.n_visual_features = n_visual_features
         
         self.feature_extractor = get_feature_extractor()
         self.lstm = nn.LSTM(input_size=n_visual_features, hidden_size=lstm_hidden_size, num_layers=1, batch_first=True)
         
-        # self.classifier = nn.Sequential(
-        #     nn.Linear(128, 64),
-        #     nn.Dropout(p=0.25),
-        #     nn.Linear(64, n_classes)
-        # )
         self.classifier = nn.Linear(128, n_classes)
-        
         self.optimizer = optim.Adam(self.parameters(), lr=0.0001)
         self.criterion = nn.NLLLoss()
         self.device = device
+
+    def init_hidden_and_cell(self):
+        # hard-coded batch size of 1
+        h0 = torch.zeros(1, 1, self.lstm_hidden_size, device=self.device)
+        c0 = torch.zeros(1, 1, self.lstm_hidden_size, device=self.device)
+        return (h0, c0)
 
     def get_classes(self, classifier_output, method=1):
         if method == 1:
@@ -72,21 +73,25 @@ class LSTMSlidingWindow(nn.Module):
         
         raise Exception()
 
-    def forward(self, snippet, hc):
-        batch, timesteps, c, h, w = snippet.size()
-
-        c_in = snippet.view(batch*timesteps, c, h, w)
+    def forward(self, video):
+        batch, timesteps, c, h, w = video.size()
+        c_in = video.view(batch*timesteps, c, h, w)
         c_out = self.feature_extractor(c_in)
-        c_out = c_out.view(batch, timesteps, self.n_visual_features)
-        
-        lstm_out, hc = self.lstm(c_out, hc)
-        
-        classifier_out = self.classifier(lstm_out[:, :, :])
-        # classifier_out.shape = (batch=1. timesteps=variable, hidden_dim=128)
+        c_out = c_out.view(batch, timesteps, -1)
 
-        # pick 1 of 4 lstm output interpretations methods
-        classes = self.get_classes(classifier_out, method=1)
+        hc = self.init_hidden_and_cell()
 
-        log_softmax = F.log_softmax(classes, dim=1)
+        # slide window with stride 1
+        for end in range(self.fps, timesteps+1, STRIDE):
+            start = end-self.fps
+            feature_sequence = c_out[:, start:end, :]
 
-        return log_softmax, hc
+            lstm_out, hc = self.lstm(feature_sequence, hc)
+            classifier_out = self.classifier(lstm_out[:, :, :])
+
+            # pick 1 of 4 interpretations methods
+            classes = self.get_classes(classifier_out, method=1)
+
+        # classify on the last block
+        softmax = F.log_softmax(classes, dim=1)
+        return softmax
